cd533bbc448389fe6ad4628f9f365f2f54263306
Ted Shaffer <shaffer.family@gmail.com>
2024-05-23 04:38:13 -0700

    UI to upload file.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { TrackerDispatch, setAppInitialized } from '../models';
+import { isNil } from 'lodash';
 
 export interface AppProps {
   onSetAppInitialized: () => any;
@@ -9,11 +10,42 @@ export interface AppProps {
 
 const App = (props: AppProps) => {
 
+  const [selectedFile, setSelectedFile] = React.useState(null);
+  const [uploadError, setUploadError] = React.useState(false);
+  const [errorList, setErrorList] = React.useState<string[]>([]);
+
+  const handleFileChangeHandler = (e: any) => {
+    setSelectedFile(e.target.files[0]);
+  };
+
+  const handleUploadFile = () => {
+    if (!isNil(selectedFile)) {
+      const data = new FormData();
+      data.append('file', selectedFile);
+      console.log('handleUploadFile, selectedFile: ', selectedFile);
+      // props.onUploadFile(data)
+      //   .then((response: any) => {
+      //     console.log(response);
+      //     console.log(response.statusText);
+      //   }).catch((err: any) => {
+      //     console.log('uploadFile returned error');
+      //     console.log(err);
+      //     // const errorList: string[] = err.response.data;
+      //     // console.log('errorList:');
+      //     // console.log(errorList);
+      //     setErrorList(err.response.data);
+      //     setUploadError(true);
+      //   });
+    }
+  };
+
   return (
     <div>
-      poo
+      <input type="file" name="file" onChange={handleFileChangeHandler} />
+      <br />
+      <button type="button" onClick={handleUploadFile}>Upload</button>
+      <br />
     </div>
-
   );
 };
 
