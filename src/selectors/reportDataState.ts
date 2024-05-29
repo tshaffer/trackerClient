8c0f03efe0f2809c38d5769b26e848158751c28d
Ted Shaffer <shaffer.family@gmail.com>
2024-05-29 10:46:01 -0700

    additional selectors.

--- src/selectors/reportDataState.ts
+++ src/selectors/reportDataState.ts
@@ -4,6 +4,18 @@ export const getReportDataState = (state: TrackerState): ReportDataState => {
   return state.reportDataState;
 };
 
+export const getStartDate = (state: TrackerState): string => {
+  return state.reportDataState.startDate;
+}
+
+export const getEndDate = (state: TrackerState): string => {
+  return state.reportDataState.endDate;
+}
+
+export const getTotal = (state: TrackerState): number => {
+  return state.reportDataState.total;
+}
+
 export const getTransactionsByCategory = (state: TrackerState): StringToTransactionsLUT => {
   return state.reportDataState.transactionsByCategory;
 };
