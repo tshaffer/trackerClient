0ead42c1214427f4bb63d02c97f04ac477e5eb4b
Ted Shaffer <shaffer.family@gmail.com>
2024-06-12 04:56:37 -0700

    Some CSS improvements, but more are needed.

--- src/components/CategoriesAccordion.tsx
+++ src/components/CategoriesAccordion.tsx
@@ -1,8 +1,21 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { Box, Accordion, AccordionSummary, AccordionDetails, Typography } from '@mui/material';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import { styled } from '@mui/system';
 
 import CategoryList from './CategoryList';
+
+const CustomAccordionSummary = styled(AccordionSummary)({
+  margin: '0px',
+  // padding: '8px 16px',
+  padding: '0px',
+});
+
+const CustomAccordionDetails = styled(AccordionDetails)({
+  // padding: '8px 16px', // Adjust the padding as needed
+  margin: '0px',
+  padding: '0px',
+});
 
 const CategoriesAccordion: React.FC = () => {
   const [maxWidth, setMaxWidth] = useState<number>(400);
@@ -22,13 +35,31 @@ const CategoriesAccordion: React.FC = () => {
     }
   }, []);
 
-  return (
-    <Box sx={{ width: '100%', maxWidth: maxWidth, bgcolor: 'background.paper' }}>
-      <Accordion>
-        <AccordionSummary expandIcon={<ArrowDropDownIcon />} id="panel2-header">
+  /*
+        <AccordionSummary expandIcon={<ArrowDropDownIcon />} id="panel2-header"
+        sx={{ margin: 0, padding: '8px 16px' }}>
           <Typography>Categories</Typography>
         </AccordionSummary>
-        <AccordionDetails>
+        <AccordionDetails sx={{ padding: '8px 16px' }}>
+          <CategoryList />
+        </AccordionDetails>
+  */
+  return (
+    <Box sx={{ width: '100%', maxWidth: maxWidth, bgcolor: 'background.paper' }}>
+      {/* <Accordion>
+        <CustomAccordionSummary expandIcon={<ArrowDropDownIcon />} id="panel2-header">
+          <Typography>Categories</Typography>
+        </CustomAccordionSummary>
+        <CustomAccordionDetails>
+          <CategoryList />
+        </CustomAccordionDetails>
+      </Accordion> */}
+      <Accordion>
+        <AccordionSummary expandIcon={<ArrowDropDownIcon />} id="panel2-header"
+        sx={{ margin: 0, padding: '0px 0px' }}>
+          <Typography>Categories</Typography>
+        </AccordionSummary>
+        <AccordionDetails sx={{ margin: 0, padding: '0px 0px' }}>
           <CategoryList />
         </AccordionDetails>
       </Accordion>
--- src/components/CategoryList.tsx
+++ src/components/CategoryList.tsx
@@ -4,6 +4,11 @@ import { connect } from 'react-redux';
 import { getCategories } from '../selectors/categoryState';
 import { CategoryEntity } from '../types';
 import { List, ListItem, ListItemText, Typography } from '@mui/material';
+import { styled } from '@mui/system';
+
+const CustomListItem = styled(ListItem)({
+  padding: '4px 0', // Adjust this value as needed
+});
 
 interface CategoryListProps {
   categoryEntities: CategoryEntity[];
@@ -12,19 +17,17 @@ interface CategoryListProps {
 const CategoryList: React.FC<CategoryListProps> = (props: CategoryListProps) => {
 
   const categoryEntities: CategoryEntity[] = props.categoryEntities
-  .map((categoryEntity: CategoryEntity) => categoryEntity)
-  .sort((a, b) => (a.keyword).localeCompare(b.keyword));
+    .map((categoryEntity: CategoryEntity) => categoryEntity)
+    .sort((a, b) => (a.keyword).localeCompare(b.keyword));
 
   return (
     <div>
       <List>
-        {categoryEntities.map((categoryEntity: CategoryEntity) => {
-          return (
-            <ListItem key={categoryEntity.id}>
-              <ListItemText primary={categoryEntity.keyword} />
-            </ListItem>
-          );
-        })}
+        {props.categoryEntities.map((categoryEntity: CategoryEntity) => (
+          <CustomListItem key={categoryEntity.id}>
+            <ListItemText primary={categoryEntity.keyword} />
+          </CustomListItem>
+        ))}
       </List>
     </div>
   );
