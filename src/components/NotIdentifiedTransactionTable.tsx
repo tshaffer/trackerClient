b76831e3e0a5589968d9ae39eec645110cb0008b
Ted Shaffer <shaffer.family@gmail.com>
2024-06-17 05:54:24 -0700

    Add NotIdentifiedTransactionTable

---
+++ src/components/NotIdentifiedTransactionTable.tsx
@@ -0,0 +1,125 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import { v4 as uuidv4 } from 'uuid';
+
+import '../styles/Tracker.css';
+import { BankTransactionEntity, BankTransactionType, CategoryKeywordEntity, CheckingAccountTransactionEntity, CreditCardTransactionEntity } from '../types';
+import { formatCurrency, formatDate } from '../utilities';
+import { IconButton } from '@mui/material';
+import AssignmentIcon from '@mui/icons-material/Assignment';
+import AddRuleDialog from './AddRuleDialog';
+import { addCategoryKeywordServerAndRedux, search } from '../controllers';
+import { TrackerDispatch } from '../models';
+import { getStartDate, getEndDate, getUnidentifiedBankTransactions } from '../selectors';
+
+interface NotIdentifiedTransactionTableProps {
+  startDate: string;
+  endDate: string;
+  unidentifiedBankTransactions: BankTransactionEntity[];
+  onAddCategoryKeyword: (categoryKeywordEntity: CategoryKeywordEntity) => any;
+  onSearch: (startDate: string, endDate: string) => any;
+}
+
+const NotIdentifiedTransactionTable: React.FC<NotIdentifiedTransactionTableProps> = (props: NotIdentifiedTransactionTableProps) => {
+
+  const [unidentifiedBankTransactionId, setUnidentifiedBankTransactionId] = React.useState('');
+  const [showAddRuleDialog, setShowAddRuleDialog] = React.useState(false);
+
+  const getTransactionTypeLabel = (bankTransactionType: BankTransactionType): string => {
+    if (bankTransactionType === BankTransactionType.CreditCard) {
+      return 'Credit Card';
+    } else {
+      return 'Checking Account';
+    }
+  }
+
+  const getTransactionDetails = (bankTransactionEntity: BankTransactionEntity): string => {
+    if (bankTransactionEntity.bankTransactionType === BankTransactionType.CreditCard) {
+      return (bankTransactionEntity as CreditCardTransactionEntity).description;
+    } else {
+      return (bankTransactionEntity as CheckingAccountTransactionEntity).name;
+    }
+  }
+
+  const handleButtonClick = (unidentifiedBankTransaction: BankTransactionEntity) => {
+    setUnidentifiedBankTransactionId(unidentifiedBankTransaction.id);
+    setShowAddRuleDialog(true);
+  };
+
+  const handleAddRule = (categoryKeyword: string, categoryId: string): void => {
+    const id: string = uuidv4();
+    const categoryKeywordEntity: CategoryKeywordEntity = {
+      id,
+      keyword: categoryKeyword,
+      categoryId
+    };
+    console.log('handleAddRule: ', categoryKeyword, categoryKeywordEntity);
+    props.onAddCategoryKeyword(categoryKeywordEntity)
+      .then(() => {
+        props.onSearch(props.startDate, props.endDate);
+      }
+    );
+  }
+
+  const handleCloseAddRuleDialog = () => {
+    setShowAddRuleDialog(false);
+  };
+
+  return (
+    <React.Fragment>
+      <AddRuleDialog
+        open={showAddRuleDialog}
+        onAddRule={handleAddRule}
+        onClose={handleCloseAddRuleDialog}
+        unidentifiedBankTransactionId={unidentifiedBankTransactionId}
+      />
+
+      <div className="table-container">
+        <div className="table-header">
+          <div className="table-row">
+            <div className="table-cell"></div>
+            <div className="table-cell">Transaction Type</div>
+            <div className="table-cell">Description</div>
+            <div className="table-cell">Date</div>
+            <div className="table-cell">Amount</div>
+          </div>
+        </div>
+        <div className="table-body">
+          {props.unidentifiedBankTransactions.map((unidentifiedBankTransaction: BankTransactionEntity) => (
+            <div className="table-row" key={unidentifiedBankTransaction.id}>
+              <div className="table-cell">
+                <IconButton onClick={() => handleButtonClick(unidentifiedBankTransaction)}>
+                  <AssignmentIcon />
+                </IconButton>
+              </div>
+              <div className="table-cell">{getTransactionTypeLabel(unidentifiedBankTransaction.bankTransactionType)}</div>
+              <div className="table-cell">{getTransactionDetails(unidentifiedBankTransaction)}</div>
+              <div className="table-cell">{formatDate(unidentifiedBankTransaction.transactionDate)}</div>
+              <div className="table-cell">{formatCurrency(-unidentifiedBankTransaction.amount)}</div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </React.Fragment>
+  );
+}
+
+function mapStateToProps(state: any) {
+  return {
+    startDate: getStartDate(state),
+    endDate: getEndDate(state),
+    unidentifiedBankTransactions: getUnidentifiedBankTransactions(state),
+  };
+}
+
+const mapDispatchToProps = (dispatch: TrackerDispatch) => {
+  return bindActionCreators({
+    onAddCategoryKeyword: addCategoryKeywordServerAndRedux,
+    onSearch: search,
+  }, dispatch);
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(NotIdentifiedTransactionTable);
+
--- src/components/ReportsContent.tsx
+++ src/components/ReportsContent.tsx
@@ -1,38 +1,29 @@
-import React, { useState } from 'react';
-import { Tabs, Tab, Box, Typography, FormControl, FormLabel, RadioGroup, FormControlLabel, Radio, MenuItem, Select, InputLabel, SelectChangeEvent, Button } from '@mui/material';
+import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { TrackerDispatch, setEndDate, setExpenseReportDateRangeType, setStartDate } from '../models';
-import { getStartDate, getEndDate, getTransactionsByCategory, getUnidentifiedBankTransactions, getExpenseReportDateRangeType } from '../selectors';
-import { StringToTransactionsLUT, BankTransactionEntity, ExpenseReportDateRangeType } from '../types';
-import dayjs, { Dayjs } from 'dayjs';
-import { isNil } from 'lodash';
-import { LocalizationProvider, DatePicker } from '@mui/x-date-pickers';
-import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+
+import { Tabs, Tab, Box, Typography, Button } from '@mui/material';
+
+import { TrackerDispatch } from '../models';
+import { getStartDate, getEndDate } from '../selectors';
 import { search } from '../controllers';
-import SpendingReportTable from './SpendingReportTable';
+
 import DateRangeSpecifier from './DateRangeSpecifier';
+import SpendingReportTable from './SpendingReportTable';
+import NotIdentifiedTransactionTable from './NotIdentifiedTransactionTable';
 
 export interface ReportsContentPropsFromParent {
   activeTab: number;
 }
 
 interface ReportsContentProps extends ReportsContentPropsFromParent {
-  expenseReportDateRangeType: ExpenseReportDateRangeType;
-  transactionsByCategory: StringToTransactionsLUT,
-  unidentifiedTransactions: BankTransactionEntity[],
   startDate: string,
   endDate: string,
-  onSetExpenseReportDateRangeType: (dateRangeType: ExpenseReportDateRangeType) => any;
-  onSetStartDate: (startDate: string) => any;
-  onSetEndDate: (endDate: string) => any;
   onSearch: (startDate: string, endDate: string) => any;
 }
 
 const ReportsContent: React.FC<ReportsContentProps> = (props: ReportsContentProps) => {
   const [tabIndex, setTabIndex] = React.useState(props.activeTab);
-  const [selectedStatement, setSelectedStatement] = useState<string>('');
 
   React.useEffect(() => {
     setTabIndex(props.activeTab);
@@ -42,79 +33,13 @@ const ReportsContent: React.FC<ReportsContentProps> = (props: ReportsContentProp
     setTabIndex(newValue);
   };
 
-  const handleDateOptionChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    props.onSetExpenseReportDateRangeType(event.target.value as ExpenseReportDateRangeType);
-  };
-
-  const handleSetStartDate = (dateDayJs: Dayjs | null) => {
-    if (!isNil(dateDayJs)) {
-      const date: Date = dateDayJs.toDate();
-      props.onSetStartDate(date.toISOString());
-    }
-  };
-
-  const handleSetEndDate = (dateDayJs: Dayjs | null) => {
-    if (!isNil(dateDayJs)) {
-      const date: Date = dateDayJs.toDate();
-      props.onSetEndDate(date.toISOString());
-    }
-  };
-
-  const handleStatementChange = (event: SelectChangeEvent<string>) => {
-    setSelectedStatement(event.target.value as string);
-  };
-
   const handleGenerateReport = () => {
-    console.log('Generating report with the following settings:');
-    console.log('Date Option:', props.expenseReportDateRangeType);
-    if (props.expenseReportDateRangeType === ExpenseReportDateRangeType.DateRange) {
-      console.log('Start Date:', props.startDate);
-      console.log('End Date:', props.endDate);
-    }
-    if (props.expenseReportDateRangeType === 'statement') {
-      console.log('Selected Statement:', selectedStatement);
-    }
-
     props.onSearch(props.startDate, props.endDate);
   };
 
-  const renderStartDate = (): JSX.Element => {
-    return (
-      <React.Fragment>
-        <FormControl style={{ marginLeft: '6px', display: 'block' }}>
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DemoContainer components={['DatePicker']}>
-              <DatePicker
-                label="Date"
-                value={dayjs(props.startDate)}
-                onChange={(newValue) => handleSetStartDate(newValue)}
-                disabled={props.expenseReportDateRangeType !== ExpenseReportDateRangeType.DateRange}
-              />
-            </DemoContainer>
-          </LocalizationProvider>
-        </FormControl>
-      </React.Fragment>
-    );
-  };
-
-  const renderEndDate = (): JSX.Element => {
-    return (
-      <React.Fragment>
-        <FormControl style={{ marginLeft: '6px', display: 'block' }}>
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DemoContainer components={['DatePicker']}>
-              <DatePicker
-                label="Date"
-                value={dayjs(props.endDate)}
-                onChange={(newValue) => handleSetEndDate(newValue)}
-                disabled={props.expenseReportDateRangeType !== ExpenseReportDateRangeType.DateRange}
-              />
-            </DemoContainer>
-          </LocalizationProvider>
-        </FormControl>
-      </React.Fragment>
-    );
-  };
+  const handleGenerateUnidentifiedTransactionsGenerateReport = () => {
+    props.onSearch(props.startDate, props.endDate);
+  }
 
   return (
     <Box sx={{ width: '100%' }}>
@@ -126,40 +51,7 @@ const ReportsContent: React.FC<ReportsContentProps> = (props: ReportsContentProp
       <Box sx={{ padding: '20px' }}>
         {tabIndex === 0 && (
           <Box>
-            <FormControl component="fieldset">
-              <FormLabel component="legend">Specify Date Range</FormLabel>
-              <RadioGroup value={props.expenseReportDateRangeType} onChange={handleDateOptionChange}>
-                <FormControlLabel value={ExpenseReportDateRangeType.All} control={<Radio />} label="All Dates" sx={{ maxHeight: '32px' }} />
-                <FormControlLabel value={ExpenseReportDateRangeType.YearToDate} control={<Radio />} label="Year to Date" sx={{ maxHeight: '32px' }} />
-                <FormControlLabel value={ExpenseReportDateRangeType.LastYear} control={<Radio />} label="Last Year" sx={{ maxHeight: '32px' }} />
-                <FormControlLabel value={ExpenseReportDateRangeType.DateRange} control={<Radio />} label="Date Range" sx={{ maxHeight: '32px' }} />
-              </RadioGroup>
-            </FormControl>
-            <Box sx={{ mt: 2, display: 'flex', gap: 2 }}>
-              <React.Fragment>
-                {renderStartDate()}
-                {renderEndDate()}
-              </React.Fragment>
-            </Box>
-            <FormControl component="fieldset" sx={{ mt: 2 }}>
-              <RadioGroup value={props.expenseReportDateRangeType} onChange={handleDateOptionChange}>
-                <FormControlLabel value={ExpenseReportDateRangeType.Statement} control={<Radio />} label="From Statement" sx={{ maxHeight: '32px' }} />
-              </RadioGroup>
-              <FormControl fullWidth disabled={props.expenseReportDateRangeType !== 'statement'} sx={{ mt: 2 }}>
-                <InputLabel id="statement-select-label">Statement</InputLabel>
-                <Select
-                  labelId="statement-select-label"
-                  id="statement-select"
-                  value={selectedStatement}
-                  onChange={handleStatementChange}
-                  label="Statement"
-                >
-                  <MenuItem value="statement1">Statement 1</MenuItem>
-                  <MenuItem value="statement2">Statement 2</MenuItem>
-                  <MenuItem value="statement3">Statement 3</MenuItem>
-                </Select>
-              </FormControl>
-            </FormControl>
+            <DateRangeSpecifier />
             <Box sx={{ mt: 3 }}>
               <Button variant="contained" color="primary" onClick={handleGenerateReport}>
                 Generate Expenses Report
@@ -170,7 +62,19 @@ const ReportsContent: React.FC<ReportsContentProps> = (props: ReportsContentProp
             </Box>
           </Box>
         )}
-        {tabIndex === 1 && <DateRangeSpecifier/>}
+        {tabIndex === 1 && (
+          <Box>
+            <DateRangeSpecifier />
+            <Box sx={{ mt: 3 }}>
+              <Button variant="contained" color="primary" onClick={handleGenerateUnidentifiedTransactionsGenerateReport}>
+                Generate Unidentified Transactions Report
+              </Button>
+            </Box>
+            <Box>
+              <NotIdentifiedTransactionTable />
+            </Box>
+          </Box>
+        )}
       </Box>
     </Box>
   );
@@ -178,19 +82,13 @@ const ReportsContent: React.FC<ReportsContentProps> = (props: ReportsContentProp
 
 function mapStateToProps(state: any) {
   return {
-    expenseReportDateRangeType: getExpenseReportDateRangeType(state),
     startDate: getStartDate(state),
     endDate: getEndDate(state),
-    transactionsByCategory: getTransactionsByCategory(state),
-    unidentifiedTransactions: getUnidentifiedBankTransactions(state),
   };
 }
 
 const mapDispatchToProps = (dispatch: TrackerDispatch) => {
   return bindActionCreators({
-    onSetExpenseReportDateRangeType: setExpenseReportDateRangeType,
-    onSetStartDate: setStartDate,
-    onSetEndDate: setEndDate,
     onSearch: search,
   }, dispatch);
 };
