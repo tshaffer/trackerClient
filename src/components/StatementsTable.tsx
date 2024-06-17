2e30ae43405e133f333f1bca27ff5b91f1cc8e27
Ted Shaffer <shaffer.family@gmail.com>
2024-06-17 10:09:21 -0700

    improved formatting of statements table.

--- src/components/StatementsTable.tsx
+++ src/components/StatementsTable.tsx
@@ -5,16 +5,17 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { isEmpty } from 'lodash';
 
-import { StatementEntity } from '../types';
+import { StatementEntity, StatementType } from '../types';
 import { TrackerDispatch } from '../models';
 import { getStatements } from '../selectors';
+import { formatDate } from '../utilities';
 
 interface StatementsTableProps {
   statements: StatementEntity[];
 }
 
 const StatementsTable: React.FC<StatementsTableProps> = (props: StatementsTableProps) => {
-  
+
   if (isEmpty(props.statements)) {
     return null;
   }
@@ -22,23 +23,27 @@ const StatementsTable: React.FC<StatementsTableProps> = (props: StatementsTableP
   return (
     <React.Fragment>
       <div className="table-container">
-      <div className="table-header">
-        <div className="table-row">
-          <div className="table-cell">Start Date</div>
-          <div className="table-cell">End Date</div>
+        <div className="table-header">
+          <div className="table-row">
+            <div className="table-cell"></div>
+            <div className="table-cell">Start Date</div>
+            <div className="table-cell">End Date</div>
+            <div className="table-cell">Type</div>
+          </div>
+        </div>
+        <div className="table-body">
+          {props.statements.map((statement: StatementEntity) => (
+            <React.Fragment key={statement.id}>
+              <div className="table-row">
+                <div className="table-cell"></div>
+                <div className="table-cell">{formatDate(statement.startDate)}</div>
+                <div className="table-cell">{formatDate(statement.endDate)}</div>
+                <div className="table-cell">{statement.type === StatementType.Checking ? 'Checking' : 'Credit Card'}</div>
+              </div>
+            </React.Fragment>
+          ))}
         </div>
       </div>
-      <div className="table-body">
-        {props.statements.map((statement: StatementEntity) => (
-          <React.Fragment key={statement.id}>
-            <div className="table-row">
-              <div className="table-cell">{statement.startDate}</div>
-              <div className="table-cell">{statement.endDate}</div>
-            </div>
-          </React.Fragment>
-        ))}
-      </div>
-    </div>
     </React.Fragment >
   );
 };
